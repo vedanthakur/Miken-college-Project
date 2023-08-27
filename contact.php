0b8da5b3fb2d9ebec4e71979e8b7597333d2e9d2
mikenthakur <mikenthakur7@gmail.com>
2023-08-27 11:04:36 +0545

    fix css

--- contact.php
+++ contact.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,28 +8,86 @@
     <link rel="stylesheet" href="css/main.css">
     <link rel="stylesheet" href="css/footer.css">
     <title>Conatct</title>
+    <style>
+    .centered-text {
+        text-align: center;
+        /* Horizontally center-align text */
+        display: block;
+        justify-content: center;
+        /* Center-align child elements */
+        align-items: center;
+        /* Vertically center-align child elements */
+        height: 100hh;
+        font-size: 20px;
+        width: 50hh;
+        padding-left:  30px;
+        padding-right: 15px;
+
+
+
+    }
+
+
+    .left-text {
+        display: flex;
+        text-align: center;
+        justify-content: center;
+        align-items: realtive;
+        height:150px;
+        padding-top: 5px;
+        padding-bottom: 5px;
+        padding-left: 33px;
+        padding-right: 0px;
+        
+        
+
+    
+        
+    }
+
+    .blink {
+        animation: blink 2s linear infinite;
+        color: red;
+    }
+
+    @keyframes blink {
+        50% {
+            opacity: 0;
+        }
+    }
+    </style>
 </head>
+
 <body>
     <?php include 'nav.php'; ?>
     <main>
-        <h1>Contact</h1>
-        <form action="inc/contact.inc.php" method="post">
-            <label for="name">Name</label>
-            <input type="text" name="name" id="name" placeholder="Enter name" required><br><br>
+        <div class="centered-text">
+            <h1 class="blink">Contact</h1><br>
+            <form action="inc/contact.inc.php" method="post">
+                <label for="name"> Name    :</label>
+                <input type="text" name="name" id="name" placeholder="        Enter your name" required><br><br>
 
-            <label for="email">Email</label>
-            <input type="email" name="email" id="email" placeholder="Enter email" required><br><br>
+                <label for="email">Email :</label>
+                <input type="email" name="email" id="email" placeholder="     Enter your email" required><br><br>
 
-            <label for="mobile">Mobile</label>
-            <input type="tel" name="mobile" id="mobile" placeholder="Enter mobile no." required><br><br>
+                <label for="mobile">Mobile :</label>
+                <input type="tel" name="mobile" id="mobile" placeholder="      Enter your mobile no" required><br><br>
 
-            <label for="message">Message</label><br>
-            <textarea name="message" id="message" cols="30" rows="10" placeholder="Enter your message" required></textarea><br><br>
+                <div class="left-text">
 
-            <input type="submit" name="submit" value="Submit">
+                    <label for="message" class="left-text"> Messages: </label>
+                    <textarea name="message" id=" message  " cols="30" rows="" placeholder="   Enter your messages "
+                        required></textarea><br><br>
+                </div>
+                
 
-        </form>
+                <input type="submit" name="submit" value="Submit">
+
+
+            </form>
+        </div>
     </main>
     <?php include 'footer.php'; ?>
 </body>
+
 </html>
--- profile.php
+++ profile.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,12 +8,120 @@
     <link rel="stylesheet" href="css/main.css">
     <link rel="stylesheet" href="css/footer.css">
     <title>Profile</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+        integrity="sha512-SfTiTlX6kk+qitfevl/7LibUOeJWlt9rbyDn92a1DqWOw9vWG2MFoays0sgObmWazO5BQPiFucnnEAjpAB+/Sw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <style>
+    .main {
+        /* background-color:yellow; */
+        width: 100vw;
+
+    }
+
+    .centerdiv {
+        font-size: 22px;
+    }
+
+    .mikenpic {
+        height: 30px;
+
+    }
+
+    .Profile {
+        display: flex;
+        justify-content: center;
+        flex-direction: column;
+
+    }
+
+    .hello {
+        font-size: 25px;
+    }
+
+    .name {
+        font-size: 50px;
+        font-weight: bold;
+    }
+
+    .moreabout {
+        font-size: 22px;
+    }
+
+    .buttons {
+        margin-top: 34px
+    }
+
+    .buttons button {
+        padding: 7px 20px;
+        border-radius: 18px;
+        color: white;
+        background-color: dodgerblue;
+        font-weight: bold;
+        font-size: 15px;
+        margin: 1 20px;
+        cursor: pointer;
+
+    }
+
+    .about {
+        font-size: 40px;
+    }
+
+    .infocontainer {
+        /* background-color: rgb(202,9,128); */
+        height: 72vh;
+        width: 80vw;
+        margin: 15px auto;
+        display: flex;
+        justify-content: space-around;
+
+    }
+    </style>
+
+
 </head>
+
 <body>
+
     <?php include 'nav.php'; ?>
+    <div class="center">
+
+    </div>
     <main>
-        
+
+        <div class="main">
+            <div class="infocontainer">
+                <div class="Profile">
+                    <div class="hello">Hi I am </div>
+                    <div class="name">Miken Thakur</div>
+                    <div class="about">Developer, Ethical Hacker</div>
+                    <div class="moreabout">I am a developer, and I love to drink coffee.</div>
+                    <div class="buttons">
+                        <form action="inc/login.inc.php" method="post">
+                            <button>Log in</button>
+
+
+                    </div>
+                </div>
+                <div class="mikenpic"><img src="miken.png" alt="Miken"></div>
+
+                <div class="centerdiv">
+                    <a href="https://www.facebook.com/miken.thakur/" target="_blank">
+                        <i class="fa fa-facebook"></i>
+                    </a>
+                    <br><br>
+                    <a href="https://www.instagram.com/miken_thakur37/" target="_blank">
+                        <i class="fa fa-instagram"></i>
+
+                    </a>
+                    <br><br>
+
+                    <a href="https://www.youtube.com/" target="_blank">
+                        <i class="fa fa-youtube"></i>
+                    </a>
+                </div>
     </main>
     <?php include 'footer.php'; ?>
 </body>
+
 </html>
